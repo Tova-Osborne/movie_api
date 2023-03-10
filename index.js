84f4332348a40beb8a585ba43cd47850dd185bce
Tova Osborne <tovaoz@gmail.com>
2023-03-10 12:28:05 -0800

    Update index.js

--- index.js
+++ index.js
@@ -11,10 +11,17 @@ const { check, validationResult } = require("express-validator");
 const app = express();
 const Movies = Models.Movie;
 const Users = Models.User;
-mongoose.connect("mongodb://localhost:27017/Movie_appdb", {
-  useNewURLParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.CONNECTION_URI,
+  {
+    useNewURLParser: true,
+    useUnifiedTopology: true,
+  }
+);
+// mongoose.connect("mongodb://localhost:27017/Movie_appdb", {
+//   useNewURLParser: true,
+//   useUnifiedTopology: true,
+// });
 const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
   flags: "a",
 });
