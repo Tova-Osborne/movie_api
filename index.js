179f5c5f5abc9cbda44296c6e735e7bc5bb46abd
Tova Osborne <tovaoz@gmail.com>
2023-03-10 15:43:10 -0800

    Update index.js

--- index.js
+++ index.js
@@ -12,17 +12,16 @@ const app = express();
 const Movies = Models.Movie;
 const Users = Models.User;
 
-// mongoose.connect(
-
-//   {
-//     useNewURLParser: true,
-//     useUnifiedTopology: true,
-//   }
-// );
-mongoose.connect("mongodb://localhost:27017/Movie_appdb", {
-  useNewURLParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+process.env.DBConnect,
+  {
+    useNewURLParser: true,
+    useUnifiedTopology: true,
+  });
+// mongoose.connect("mongodb://localhost:27017/Movie_appdb", {
+//   useNewURLParser: true,
+//   useUnifiedTopology: true,
+// });
 const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
   flags: "a",
 });
