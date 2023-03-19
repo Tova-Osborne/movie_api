b2b1d4ecfe0c8c8af3a205d2bb063d5e9fab5646
Tova Osborne <tovaoz@gmail.com>
2023-03-19 12:29:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -28,21 +28,21 @@ app.use(bodyParser.json());
 app.use(morgan("combined", { stream: accessLogStream }));
 
 const cors = require("cors");
-let allowedOrigins = ["*"];
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        let message =
-          "The CORS policy for this application doesn't allow access from origin" +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+Access-Control-Allow-Origin: *
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         let message =
+//           "The CORS policy for this application doesn't allow access from origin" +
+//           origin;
+//         return callback(new Error(message), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
 
 let auth = require("./auth")(app);
 const passport = require("passport");
